2d80ffd40097899013493f5a8f79e7173ca2c7c0
lex-quarkie <lex.quarkie@gmail.com>
2019-01-07 18:56:18 +0300

    Add obj_count

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+from itertools import count
+
 class Receipt(object):
     """Блюдо - название должно быть уникально"""
+
+    _ids = count(1)
+
     def __init__(self, name, ingredients, text):
         self.name = name
         self.ingredients = ingredients
         self.text = text
+        self.id = next(self._ids)
 
     def view(self):
+        print('Рецепт #{}'.format(self.id))
         print('{}'.format(self.name))
         print('Ингредиенты: {}'.format(self.ingredients))
-        print('{}'.format(self.text))
+        print('{}'.format(self.text+'\n'))
 
 def main():
     test_receipt_name = 'Омлет'
+    test_receipt_name_2 = 'Омлет 2'
     test_ingredients = ["3 яйца", "столовая ложка молока", "соль", "перец", "масло сливочное"]
     test_receipt_text = """
 Вылейте яйца в тарелку, добавьте столовую ложку
@@ -23,9 +31,9 @@ def main():
 При желании, добавьте в омлет тертый сыр, 
 приправы и другие добавки(грибы, лук, сладкий, перец, спаржа, др.)."""
     test_receipt = Receipt(test_receipt_name, test_ingredients, test_receipt_text)
+    test_receipt_2 = Receipt(test_receipt_name_2, test_ingredients, test_receipt_text)
     test_receipt.view()
-
-
+    test_receipt_2.view()
 
 if __name__ == '__main__':
     main()
